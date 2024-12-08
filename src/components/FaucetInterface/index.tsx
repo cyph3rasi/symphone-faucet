350ac2a74c6d0c7ea0ecee168a7724e39fc3089f
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-07 23:04:18 -0800

    Update FaucetInterface to use available components

--- src/components/FaucetInterface/index.tsx
+++ src/components/FaucetInterface/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useWallet, useFaucet } from './hooks';
-import { 
-  StarBackground,
-  AboutToken,
-  ClaimInterface,
-  Instructions,
-  TokenSetup 
-} from './components';
+import { useWallet } from './hooks/useWallet';
+import { useFaucet } from './hooks/useFaucet';
+import { ClaimInterface } from './components/ClaimInterface';
+import { Instructions } from './components/Instructions';
+import { TokenSetup } from './components/TokenSetup';
 
-const FaucetInterface: React.FC = () => {
+const FaucetInterface = () => {
   const { account, loading: walletLoading, error: walletError, connectWallet } = useWallet();
   const { 
     loading: faucetLoading, 
@@ -22,10 +19,8 @@ const FaucetInterface: React.FC = () => {
   return (
     <>
       {showConfetti && <Confetti />}
-      <StarBackground />
       <div className="min-h-screen py-12 px-4 relative">
         <div className="max-w-[90rem] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-6">
-          <AboutToken />
           <ClaimInterface 
             account={account}
             loading={walletLoading || faucetLoading}
