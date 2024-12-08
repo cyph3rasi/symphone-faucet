6a1918bb845d301299c6fda5e517acb50c6507c5
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-07 23:09:05 -0800

    Add react-confetti integration with proper configuration

--- src/components/FaucetInterface/index.tsx
+++ src/components/FaucetInterface/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactConfetti from 'react-confetti';
 import { useWallet } from './hooks/useWallet';
 import { useFaucet } from './hooks/useFaucet';
 import { ClaimInterface } from './components/ClaimInterface';
@@ -18,7 +19,13 @@ const FaucetInterface = () => {
 
   return (
     <>
-      {showConfetti && <Confetti />}
+      {showConfetti && <ReactConfetti
+        width={window.innerWidth}
+        height={window.innerHeight}
+        recycle={false}
+        numberOfPieces={200}
+        gravity={0.3}
+      />}
       <div className="min-h-screen py-12 px-4 relative">
         <div className="max-w-[90rem] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-6">
           <ClaimInterface 
