08cfab67b5ff6d12b3f7385e98ef17e025d10f50
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-08 01:07:45 -0800

    Integrate StarBackground component into main interface

--- src/components/FaucetInterface/index.tsx
+++ src/components/FaucetInterface/index.tsx
@@ -6,6 +6,8 @@ import { Instructions } from './components/Instructions';
 import { TokenSetup } from './components/TokenSetup';
 import { AboutToken } from './components/AboutToken';
 import { HeaderLogo } from './components/HeaderLogo';
+import { StarBackground } from './components/StarBackground';
+import '../styles/stars.css';
 
 const FaucetInterface: React.FC = () => {
   const { account, loading: walletLoading, error: walletError, connectWallet } = useWallet();
@@ -20,31 +22,34 @@ const FaucetInterface: React.FC = () => {
 
   return (
     <div className="min-h-screen py-6 px-4 relative">
-      <HeaderLogo />
-      <div className="max-w-[90rem] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-6 mt-8">
-        {/* Left Column - About Section (3 columns wide) */}
-        <div className="order-3 lg:order-1 lg:col-span-3">
-          <AboutToken />
-        </div>
+      <StarBackground />
+      <div className="content-wrapper">
+        <HeaderLogo />
+        <div className="max-w-[90rem] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-6 mt-8">
+          {/* Left Column - About Section (3 columns wide) */}
+          <div className="order-3 lg:order-1 lg:col-span-3">
+            <AboutToken />
+          </div>
 
-        {/* Middle Column - Main Interface and Instructions (6 columns wide) */}
-        <div className="order-1 lg:order-2 lg:col-span-6 space-y-6">
-          <ClaimInterface 
-            account={account}
-            loading={walletLoading || faucetLoading}
-            error={walletError || faucetError}
-            success={success}
-            showConfetti={showConfetti}
-            onConnect={connectWallet}
-            onClaim={claimTokens}
-            onClaimAgain={handleClaimAgain}
-          />
-          <Instructions />
-        </div>
+          {/* Middle Column - Main Interface and Instructions (6 columns wide) */}
+          <div className="order-1 lg:order-2 lg:col-span-6 space-y-6">
+            <ClaimInterface 
+              account={account}
+              loading={walletLoading || faucetLoading}
+              error={walletError || faucetError}
+              success={success}
+              showConfetti={showConfetti}
+              onConnect={connectWallet}
+              onClaim={claimTokens}
+              onClaimAgain={handleClaimAgain}
+            />
+            <Instructions />
+          </div>
 
-        {/* Right Column - Token Setup (3 columns wide) */}
-        <div className="order-2 lg:order-3 lg:col-span-3">
-          <TokenSetup key={`token-setup-container-${Date.now()}`} />
+          {/* Right Column - Token Setup (3 columns wide) */}
+          <div className="order-2 lg:order-3 lg:col-span-3">
+            <TokenSetup key={`token-setup-container-${Date.now()}`} />
+          </div>
         </div>
       </div>
     </div>
