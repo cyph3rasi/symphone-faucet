b733af30f950d717a887013105827f656f7f1972
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-08 01:30:47 -0800

    Add TOKEN_ABI with balanceOf method for balance checking

--- src/components/FaucetInterface/constants.ts
+++ src/components/FaucetInterface/constants.ts
@@ -7,3 +7,13 @@ export const FAUCET_ABI = [
     "function setTokenAddress(address _tokenAddr) external",
     "function setFaucetDripAmount(uint256 _amount) external"
 ];
+
+export const TOKEN_ABI = [
+    "function balanceOf(address account) view returns (uint256)",
+    "function decimals() view returns (uint8)",
+    "function symbol() view returns (string)",
+    "function name() view returns (string)",
+    "function transfer(address to, uint256 amount) returns (bool)",
+    "function allowance(address owner, address spender) view returns (uint256)",
+    "function approve(address spender, uint256 amount) returns (bool)"
+];
