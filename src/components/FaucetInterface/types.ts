3d29c6d20c8e78d247453354463f73b258691d37
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-07 21:48:40 -0800

    Add type definitions

---
+++ src/components/FaucetInterface/types.ts
@@ -0,0 +1,25 @@
+export interface WalletState {
+  account: string;
+  loading: boolean;
+  error: string;
+  connectWallet: () => Promise<void>;
+}
+
+export interface FaucetState {
+  loading: boolean;
+  error: string;
+  success: string;
+  showConfetti: boolean;
+  claimTokens: () => Promise<void>;
+  handleClaimAgain: () => void;
+}
+
+export interface ClaimInterfaceProps {
+  account: string;
+  loading: boolean;
+  error: string;
+  success: string;
+  onConnect: () => Promise<void>;
+  onClaim: () => Promise<void>;
+  onClaimAgain: () => void;
+}
