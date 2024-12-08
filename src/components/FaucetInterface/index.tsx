8dd71dcb072235a0bd99e5bcf78f9490c4e95d3c
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-07 23:53:46 -0800

    Add AboutToken component and fix layout alignment

--- src/components/FaucetInterface/index.tsx
+++ src/components/FaucetInterface/index.tsx
@@ -4,6 +4,7 @@ import { useFaucet } from './hooks/useFaucet';
 import { ClaimInterface } from './components/ClaimInterface';
 import { Instructions } from './components/Instructions';
 import { TokenSetup } from './components/TokenSetup';
+import { AboutToken } from './components/AboutToken';
 
 const FaucetInterface: React.FC = () => {
   const { account, loading: walletLoading, error: walletError, connectWallet } = useWallet();
@@ -19,19 +20,33 @@ const FaucetInterface: React.FC = () => {
   return (
     <div className="min-h-screen py-12 px-4 relative">
       <div className="max-w-[90rem] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-6">
-        <ClaimInterface 
-          account={account}
-          loading={walletLoading || faucetLoading}
-          error={walletError || faucetError}
-          success={success}
-          onConnect={connectWallet}
-          onClaim={claimTokens}
-          onClaimAgain={handleClaimAgain}
-        />
-        <div className="lg:col-span-6">
+        {/* Main claim interface - 5 columns */}
+        <div className="lg:col-span-5">
+          <ClaimInterface 
+            account={account}
+            loading={walletLoading || faucetLoading}
+            error={walletError || faucetError}
+            success={success}
+            onConnect={connectWallet}
+            onClaim={claimTokens}
+            onClaimAgain={handleClaimAgain}
+          />
+        </div>
+
+        {/* Instructions section - 4 columns */}
+        <div className="lg:col-span-4">
           <Instructions />
         </div>
-        <TokenSetup key={`token-setup-container-${Date.now()}`} />
+
+        {/* About Token sidebar - 3 columns */}
+        <div className="lg:col-span-3">
+          <AboutToken />
+        </div>
+
+        {/* Token Setup section - full width */}
+        <div className="lg:col-span-12">
+          <TokenSetup key={`token-setup-container-${Date.now()}`} />
+        </div>
       </div>
     </div>
   );
