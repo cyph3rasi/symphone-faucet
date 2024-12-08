0e80a7702579dcc26ce18bb3578055a7884baf0a
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-08 00:11:38 -0800

    Update column proportions and remove tutorial section

--- src/components/FaucetInterface/index.tsx
+++ src/components/FaucetInterface/index.tsx
@@ -20,13 +20,13 @@ const FaucetInterface: React.FC = () => {
   return (
     <div className="min-h-screen py-12 px-4 relative">
       <div className="max-w-[90rem] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-6">
-        {/* Left Column - About Section (2 columns wide) */}
-        <div className="order-3 lg:order-1 lg:col-span-2">
+        {/* Left Column - About Section (3 columns wide) */}
+        <div className="order-3 lg:order-1 lg:col-span-3">
           <AboutToken />
         </div>
 
-        {/* Middle Column - Main Interface and Instructions (8 columns wide) */}
-        <div className="order-1 lg:order-2 lg:col-span-8 space-y-6">
+        {/* Middle Column - Main Interface and Instructions (6 columns wide) */}
+        <div className="order-1 lg:order-2 lg:col-span-6 space-y-6">
           <ClaimInterface 
             account={account}
             loading={walletLoading || faucetLoading}
@@ -39,8 +39,8 @@ const FaucetInterface: React.FC = () => {
           <Instructions />
         </div>
 
-        {/* Right Column - Token Setup (2 columns wide) */}
-        <div className="order-2 lg:order-3 lg:col-span-2">
+        {/* Right Column - Token Setup (3 columns wide) */}
+        <div className="order-2 lg:order-3 lg:col-span-3">
           <TokenSetup key={`token-setup-container-${Date.now()}`} />
         </div>
       </div>
